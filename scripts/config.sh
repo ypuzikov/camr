92b686cc22debf7698a9233addc78c21741d8a8f
gemoku <puzikov@ukp.informatik.tu-darmstadt.de>
2018-02-14 17:46:02 +0100

    added smatch to the config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Models of the Stanford tools used
+# Versions of the Stanford tools used
 CORENLP_DIR="stanford-corenlp-full-2015-04-20"
 STANFORD_PARSER_DIR="stanford-parser-full-2014-01-04"
 
+# Smatch scoring script URL
+SMATCH_URL="http://alt.qcri.org/semeval2016/task8/data/uploads/smatch-v2.0.2.tar.gz"
+
+# 1. Get CoreNLP
 echo "Setup Stanford CoreNLP ..."
 cd stanfordnlp
 
@@ -17,5 +21,13 @@ if [ ! -d "$STANFORD_PARSER_DIR" ]; then
     unzip stanford-parser-full-2014-01-04.zip
 fi
 
+# 2. Get Charniak Parser
 echo "Setup Charniak Parser ..."
 pip install --user bllipparser
+
+# 3. Get Smatch scoring script
+echo "Donwloading Smatch scoring script..."
+wget $SMATCH_URL
+tar zxvf smatch-v2.0.2.tar.gz
+
+echo "Done!"
